069db7f376f6f97925610630adde88d11bfd1a9c
Michael Moreno <mmoreno2222@gmail.com>
2020-08-13 20:52:10 -0700

    Updating icons for map

--- mapScript.js
+++ mapScript.js
@@ -25,20 +25,44 @@ map.on("load", function(){
     url: routingURL,
 
     success: function (response) {
+        // Pulling info from website data
         map.addSource("route", {"type": "geojson", 'data': response});
+        // Adds the routing line onto the map
         map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'route',
-        'layout': {
-        'line-join': 'round',
-        'line-cap': 'round'
-        },
-        'paint': {
-        'line-color': '#888',
-        'line-width': 8
-        }
-    });
+            'id': 'route',
+            'type': 'line',
+            'source': 'route',
+            'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+            },
+            'paint': {
+            'line-color': '#00B8E6',
+            'line-width': 8
+            }
+        });
+
+        map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', function(error, image) {
+            if (error) throw error;
+            map.addImage('custom-marker', image);
+            map.addSource('points', {"type": "geojson", "data": response});
+            map.addLayer({
+                'id': 'points',
+                'type': 'symbol',
+                'source': 'points',
+                'layout': {
+                'icon-image': 'custom-marker',
+                // get the title name from the source's "title" property
+                'text-field': ['get', 'title'],
+                'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+                ],
+                'text-offset': [0, 1.25],
+                'text-anchor': 'top'
+                }
+            });
+        })
         console.log(response); // Full Object
         //Calculate distance
         var travelDistance = response.features[0].properties.distance;
